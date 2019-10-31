35d48b760c385c634a24540bfb7c4e9132e81551
Martin Perren <martinperren@gmail.com>
2019-10-31 01:41:40 -0300

    Update bot.js
    fix dia no ajustado a timezone argentina

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function hora(){
      hour = (hour < 10 ? "0" : "") + hour;
 var min  = date.getMinutes();
    min = (min < 10 ? "0" : "") + min;
-return 21 + ":" + 39;
+return hour + ":" + min;
 }
 
 
