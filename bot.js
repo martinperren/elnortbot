8cdc0bc1fea4fc29c00428f2a3630806ce31749c
Martin Perren <martinperren@gmail.com>
2019-12-06 01:57:52 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -172,11 +172,10 @@ bot.on('message', (msg) => {
 	 
 	 
 
-
-$(".ListView1_ctrl2_ctl00_lblSalida fh").each(() => {
-    console.log($(this).val());
-});
-
+const $ = require('cheerio')
+$('.grillaservicios_ctl02_lblSalida').find('fh').each((i,op) => {
+   console.log($(op).text())
+})
 
 });
 
